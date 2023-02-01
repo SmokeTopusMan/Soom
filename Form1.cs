1bd118d5551e00bb5b7eb37273c6614003526014
Amir Sorani <76660629+SmokeTopusMan@users.noreply.github.com>
2023-02-01 10:51:29 +0200

    Add files via upload
    tried to get the userinfo

---
+++ Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Soom_Client
+{
+    public partial class Form1 : Form
+    {
+        Queue<string> info= new Queue<string>();
+        public Form1()
+        {
+            InitializeComponent();
+            registerClick.Hide();
+            loginClick.Hide();
+            //getInfo();
+        }
+
+        private void register_Click(object sender, EventArgs e)
+        {
+            registerClick.Show();
+            loginClick.Hide();
+        }
+
+        private void login_Click(object sender, EventArgs e)
+        {
+            loginClick.Show();
+            registerClick.Hide();
+        }
+/*
+        private void getInfo()
+        {
+            Thread log = new Thread(new ThreadStart(() => loginClick.IsInfo(this.info)));
+            Thread reg = new Thread(new ThreadStart(() => registerClick.IsInfo(this.info)));
+            reg.Start();
+            log.Start();
+            while (log.IsAlive || reg.IsAlive) 
+            {
+                if (!log.IsAlive)
+                    reg.Abort();
+                else if(!reg.IsAlive)
+                    log.Abort();
+            }
+            MessageBox.Show("Yay Works!");
+        }
+*/
+    }
+}
---
+++ RegisterClick.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Soom_Client
+{
+    public partial class RegisterClick : UserControl
+    {
+        public string userInfo { get; private set; }
+        public RegisterClick()
+        {
+            InitializeComponent();
+        }
+
+        private void femaleCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (femaleCheckBox.Checked)
+                maleCheckBox.Checked = false;
+        }
+
+        private void maleCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (maleCheckBox.Checked)
+                femaleCheckBox.Checked = false;
+        }
+
+        private void ageTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+                e.Handled = true;
+        }
+
+        private void registerSubmitBtn_Click(object sender, EventArgs e)
+        {
+            if (CheckAllBoxes())
+            {
+                string sex = "";
+                if (maleCheckBox.Checked)
+                    sex += "sex:M";
+                else
+                    sex += "sex:F";
+                this.userInfo += $"REG#username:{usernameRegTextBox.Text}#password:{passRegTextBox.Text}#age:{ageTextBox.Text}" +
+                                 $"#sex:{sex}#bio:{bioTextBox.Text}";
+            }
+            else
+            {
+                if (usernameRegTextBox.Text == "")
+                    MessageBox.Show("You need to choose a username!");
+                else if (passRegTextBox.Text == "")
+                    MessageBox.Show("You need to choose a password!");
+                else if (ageTextBox.Text == "")
+                    MessageBox.Show("You need to enter an age!");
+                else
+                    MessageBox.Show("You need to choose your sex!");
+            }
+        }
+
+        private bool CheckAllBoxes()
+        {
+            return (usernameRegTextBox.Text != "" && passRegTextBox.Text != "" && ageTextBox.Text != "" && (maleCheckBox.Checked || femaleCheckBox.Checked));
+        }
+
+        public void IsInfo(Queue<string> info)
+        {
+            while (this.userInfo == null)
+            {
+                System.Threading.Thread.Sleep(1000);
+            }
+            info.Enqueue(this.userInfo);
+        }
+    }
+}
