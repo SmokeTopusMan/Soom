7dcb31363578e1bb74df87d8bd2afe7051f70ff6
Amir Sorani <76660629+SmokeTopusMan@users.noreply.github.com>
2023-02-12 22:49:46 +0200

    Add files via upload
    Uploaded the full project - server side

---
+++ Soom server/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Soom_server
+{
+    internal class User
+    {
+        public string _name { get; set; }
+        public string _password { get; set; }
+        public Socket _socket { get; private set; }
+
+        public User(Socket socket)
+        {
+            _socket = socket;
+        }
+    }
+
+}
